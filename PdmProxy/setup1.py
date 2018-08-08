69bb0448bf43a93a96aa88f909c843ac16fbaca3
charyn bryant <357346640@qq.com>
2018-08-08 11:32:15 +0800

    优化

--- PdmProxy/setup1.py
+++ PdmProxy/setup1.py
@@ -6,6 +6,6 @@ mfcdir = 'E:\python27\Lib\site-packages\pythonwin'
 mfcfiles = [os.path.join(mfcdir, i) for i in ["mfc90.dll", "mfc90u.dll", "mfcm90.dll", "mfcm90u.dll", "Microsoft.VC90.MFC.manifest"]]
 data_files = [("Microsoft.VC90.MFC", mfcfiles),
               ]
-setup(console=[{"script": "Main.py", "icon_resources": [(0, "C:\Users\Administrator\PycharmProjects\PdmProxy\PdmProxy\logo.ico")]}],
+setup(console=[{"script": "Main.py", "icon_resources": [(0, "logo.ico")]}],
       data_files=data_files,
       )
